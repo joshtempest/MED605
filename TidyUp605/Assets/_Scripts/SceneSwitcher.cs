11a6a8cfa58bd4e4ae793c2e06cedfe02cf32118
gigrove <gsoren23@student.aau.dk>
2026-03-25 15:04:41 +0100

    A fork in the road
    Added a level select system

---
+++ TidyUp605/Assets/_Scripts/SceneSwitcher.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+   
+    public void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    
+}

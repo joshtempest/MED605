55adc50c04befbaceb05fae4b74d2517fdeebc82
joshtempest <joshuabussink29@gmail.com>
2026-03-30 14:27:54 +0200

    Add receiverManager script and scene receivers
    Add a new receiverManager MonoBehaviour (receiverManager.cs + .meta) that implements three receiver types (Service, Beskidt, Mad). The script handles OnCollisionEnter to destroy objects with matching tags and warns/disables itself if multiple or no receiver types are assigned; scoring logic is left as TODO comments. Update TrialScene.unity to add several Cube GameObjects configured as receivers to test the functionality of the receiverManager script

---
+++ TidyUp605/Assets/_Scripts/receiverManager.cs
@@ -0,0 +1,94 @@
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class receiverManager : MonoBehaviour
+{
+    private GameObject GameManager;
+    private objectSpawner spawnerScript;
+
+    [SerializeField] private bool isServiceReceiver;
+    [SerializeField] private bool isBeskidtReceiver;
+    [SerializeField] private bool isMadReceiver;
+    private int receiverTypeCount;
+
+    private void Awake()
+    {
+        spawnerScript = GameObject.FindGameObjectWithTag("GameController").GetComponent<objectSpawner>();
+    }
+
+    private void Start()
+    {
+        receiverTypeCount = (isServiceReceiver ? 1 : 0) + (isBeskidtReceiver ? 1 : 0) + (isMadReceiver ? 1 : 0);
+        if (receiverTypeCount > 1)
+        {
+            Debug.LogWarning((this.gameObject) + " has " + receiverTypeCount + " types assigned. Please assign only one type to the receiver.");
+            this.gameObject.GetComponent<receiverManager>().enabled = false;
+        }
+        if (!isServiceReceiver && !isBeskidtReceiver && !isMadReceiver)
+        {
+            Debug.LogWarning((this.gameObject) + " has no type assigned. Please assign a type to the receiver.");
+            this.gameObject.GetComponent<receiverManager>().enabled = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (isServiceReceiver) 
+        { 
+            if (collision.gameObject.tag == "Service")
+            {
+                Destroy(collision.gameObject);
+                //Add score for positive interaction
+            }
+            else if (collision.gameObject.tag == "Beskidt")
+            {
+                Destroy(collision.gameObject);
+                //Subtract score for negative interaction
+            }
+            else if (collision.gameObject.tag == "Mad")
+            {
+                Destroy(collision.gameObject);
+                //Subtract score for negative interaction
+            }
+        }
+        if (isBeskidtReceiver) 
+        { 
+            if(collision.gameObject.tag == "Beskidt")
+            {
+                Destroy(collision.gameObject);
+                //Add score for positive interaction
+            }
+            else if (collision.gameObject.tag == "Service")
+            {
+                Destroy(collision.gameObject);
+                //Subtract score for negative interaction
+            }
+            else if (collision.gameObject.tag == "Mad")
+            {
+                Destroy(collision.gameObject);
+                //Subtract score for negative interaction
+            }
+        }
+        if (isMadReceiver)
+        { 
+            if(collision.gameObject.tag == "Mad")
+            {
+                Destroy(collision.gameObject);
+                //Add score for positive interaction
+            }
+            else if (collision.gameObject.tag == "Service")
+            {
+                Destroy(collision.gameObject);
+                //Subtract score for negative interaction
+            }
+            else if (collision.gameObject.tag == "Beskidt")
+            {
+                Destroy(collision.gameObject);
+                //Subtract score for negative interaction
+            }
+        }
+    }
+
+}

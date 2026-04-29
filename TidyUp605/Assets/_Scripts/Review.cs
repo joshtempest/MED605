03e03e0a483902173e4909ae9909df5eb3476a75
gigrove <gsoren23@student.aau.dk>
2026-04-29 15:00:27 +0200

    we should've stuck to overhead projectors
    fighting the blackboard

--- TidyUp605/Assets/_Scripts/Review.cs
+++ TidyUp605/Assets/_Scripts/Review.cs
@@ -1,7 +1,10 @@
+using System;
+using System.Collections.Generic; 
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.Windows; 
 
-public class TestReview : MonoBehaviour
+public class Review : MonoBehaviour
 {
     //assign in inspector
 
@@ -13,21 +16,48 @@ public class TestReview : MonoBehaviour
     public GameObject scoreBar;
 
     //stars 
-    public GameObject starOneYellow;
-    public GameObject starOneGrey;
-    public GameObject starTwoYellow;
-    public GameObject starTwoGrey;
-    public GameObject starThreeYellow;
-    public GameObject starThreeGrey;
+    public GameObject starOne;
+    public GameObject starTwo;
+    public GameObject starThree;
+
+    SpriteRenderer SROne;
+    SpriteRenderer SRTwo;
+    SpriteRenderer SRThree;
+
+    public Sprite yellowStar;
+    public Sprite greyStar;
 
     //log display (=showing what they did right/wrong)
     public GameObject itemRenderer;
     public GameObject receptacleRenderer;
     public GameObject rightWrongIcon;
 
+    public Sprite renGaffel;
+    public Sprite beskidtGaffel;
+    public Sprite renTallerken;
+    public Sprite beskidtTallerken;
+
+    public Sprite cupboard;
+    public Sprite dishwasher;
+    public Sprite fridge;
+
+    SpriteRenderer itemSR;
+    SpriteRenderer recepSR;
+    SpriteRenderer rightwrongSR;
+
+
+    //debugging only
+    private List<Tuple<string, string, bool>> testLog = new();
+
     void Start()
     {
-        
+        SROne = starOne.GetComponent<SpriteRenderer>();
+        SRTwo = starTwo.GetComponent<SpriteRenderer>();
+        SRThree = starThree.GetComponent<SpriteRenderer>();
+
+        itemSR = itemRenderer.GetComponent<SpriteRenderer>();
+        recepSR = receptacleRenderer.GetComponent<SpriteRenderer>();
+        rightwrongSR = rightWrongIcon.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -35,7 +65,70 @@ public class TestReview : MonoBehaviour
     {
         if (UnityEngine.Input.GetKeyDown(KeyCode.T))
         {
-            //run the test code
+            
         }
+    }
+
+    public void ClearBoard()
+    {
+        Review_start.gameObject.SetActive(false);
+        Review_end.gameObject.SetActive(false);
+    }
+
+    public void DisplayResults(List<Tuple<string, string, bool>> answers)
+    {
+        //*disable movement & click
+        //*move blackboard forward
+        //*sound effect?
+        //*narrator: Godt klaret!
+
+        //set stars to grey
+        SROne.sprite = greyStar;
+        SRTwo.sprite = greyStar;
+        SRThree.sprite = greyStar;
+
+
+
+        Review_start.gameObject.SetActive(true);
+        Review_end.gameObject.SetActive(false);
+
+        for (int i = 0; i < answers.Count; i++)
+        {
+            
+            //display the right object sprite
+            switch (answers[i].Item1)
+            {
+                //ren gaffel
+                case "rG":
+                    itemSR.sprite = renGaffel;
+                    break;
+                //beskidt gaffel
+                case "bG":
+                    itemSR.sprite = beskidtGaffel;
+                    break;
+
+                //ren Tallerken
+                case "rT":
+                    itemSR.sprite = renTallerken;
+                    break;
+                case "bT":
+                    itemSR.sprite = beskidtTallerken;
+                    break;
+
+                default:
+                    break;
+            }
+
+            //display the right receptacle sprite
+            switch (answers[i].Item2)
+            {
+                case ""
+                default:
+                    break;
+            }
+            
+                
+        }
+
     }
 }

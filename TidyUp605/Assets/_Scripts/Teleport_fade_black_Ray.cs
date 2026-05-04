ff67745768115795cfd55b40670f6b93f6d3c6f9
Slente468 <GL51HW@student.aau.dk>
2026-05-04 07:59:06 +0200

    snap turn works but, no fade
    teleportation on XR origin but not build for it yet. current problem ray not appearing

---
+++ TidyUp605/Assets/_Scripts/Teleport_fade_black_Ray.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion.Teleportation;
+
+public class Teleport_fade_black_Ray : TeleportationProvider
+
+{
+    // find the comaands for teleporting ak metods and put it in if x OR y is executed then ply fde
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
